7f234bc28b9f799efc8a8c0a353f6ca022652fc9
Ankit Patel <Itsakppatel@gmail.com>
2022-12-11 19:32:15 +0530

    Add files via upload

---
+++ Day2/DataType.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int a =4;
+    // int b =5;
+    int a =4,b =5;
+    cout<<"This is day 2. "<<"The variable a value is: "<<a<<endl;
+    return 0;
+}
