faa132c14a2b30cae35ba6c8958fdd0785d23d4c
Ankit Patel <Itsakppatel@gmail.com>
2022-12-11 19:41:46 +0530

    Add files via upload

--- Day2/DataType.cpp
+++ Day2/DataType.cpp
@@ -5,6 +5,10 @@ int main(){
     // int a =4;
     // int b =5;
     int a =4,b =5;
+    float pi =3.14;
+    char c ='a';
     cout<<"This is day 2. "<<"The variable a value is: "<<a<<endl;
+    cout<<"The value of pi is : "<<pi<<endl ;
+    cout<<"The value of c is : "<<c<<endl ;
     return 0;
 }
