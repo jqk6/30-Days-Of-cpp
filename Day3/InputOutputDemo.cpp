1bf978c22311b5033f0b2ea5537662f7d04506e8
Ankit Patel <Itsakppatel@gmail.com>
2022-12-12 14:22:48 +0530

    Add files via upload

---
+++ Day3/InputOutputDemo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"Enter the value of num1: \n";
+    cin>>num1;
+
+    cout<<"Enter the value of num2: \n";
+    cin>>num2;
+
+    cout<<"The sum is:"<<num1+num2<<endl;
+    return 0;
+}
+
+// '<<'  are called insertion operator
+// '>>' are called extraction operator 
