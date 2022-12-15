c06f2e160cd7d024d70a0c58d748dbccb32433b3
Ankit Patel <Itsakppatel@gmail.com>
2022-12-15 19:17:02 +0530

    Add files via upload

---
+++ Day6/ifelseExample.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter your age "<<endl;
+    cin>>age;
+    if(age<18){
+        cout<<"You are not eligible for voting."<<endl;
+    }else{
+        cout<<"You are eligible for voting."<<endl;
+    }
+    return 0;
+}
