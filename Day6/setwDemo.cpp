a1e5535fc93f89991df720e0ddb9af77f366cb5f
Ankit Patel <Itsakppatel@gmail.com>
2022-12-15 17:50:22 +0530

    Add files via upload

---
+++ Day6/setwDemo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+    int w = 1, x = 22, y = 333, z = 4444;
+    cout << "The value of w is :" << setw(4) << w << endl;
+    cout << "The value of x is :" << setw(4) << x << endl;
+    cout << "The value of y is :" << setw(4) << y << endl;
+    cout << "The value of z is :" << setw(4) << z << endl;
+    return 0;
+}
