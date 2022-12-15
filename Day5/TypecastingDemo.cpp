85dfacbad52f1f08f63ffef111740d11a024e473
Ankit Patel <Itsakppatel@gmail.com>
2022-12-15 16:41:18 +0530

    Add files via upload

--- Day5/TypecastingDemo.cpp
+++ Day5/TypecastingDemo.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-int a = 45;
-float b = 55.67;
-cout<<"The value of a is : "<<(double)a<<endl;
-cout<<"The value of a is : "<<double(a)<<endl;//Same as above line
+    int a = 45;
+    float b = 55.67;
+    cout << "The value of a is : " << (double)a << endl;
+    cout << "The value of a is : " << double(a) << endl; // Same as above line
 
+    cout << "The value of b is : " << (int)b << endl;
+    cout << "The value of b is : " << int(b) << endl; // Same as above line
 
-cout<<"The value of b is : "<<(int)b<<endl;
-cout<<"The value of b is : "<<int(b)<<endl;//Same as above line
-return 0;
+    int c = (int)b;
+
+    cout << "The expression is " << a + b << endl;
+    cout << "The expression is " << a + (int)b << endl;
+    cout << "The expression is " << a + int(b) << endl;
+
+    return 0;
 }
