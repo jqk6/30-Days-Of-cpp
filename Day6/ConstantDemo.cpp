91e5c5359e20cae725c2ea6565dfd256bcefae04
Ankit Patel <Itsakppatel@gmail.com>
2022-12-15 17:10:52 +0530

    Add files via upload

---
+++ Day6/ConstantDemo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// int a = 45;
+// cout<<"The value of a : " << a <<endl;
+// a = 55;
+
+    const int a =45;
+    cout<<"The value of a : " << a <<endl;
+    // a =43; //This will throw error because we cannot change the value of a here
+    //because now 'a' is constant 
+return 0;
+}
