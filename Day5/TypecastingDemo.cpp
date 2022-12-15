d3d456554098ac9574487135be4e5f593064e012
Ankit Patel <Itsakppatel@gmail.com>
2022-12-15 16:36:55 +0530

    Add files via upload

---
+++ Day5/TypecastingDemo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int a = 45;
+float b = 55.67;
+cout<<"The value of a is : "<<(double)a<<endl;
+cout<<"The value of a is : "<<double(a)<<endl;//Same as above line
+
+
+cout<<"The value of b is : "<<(int)b<<endl;
+cout<<"The value of b is : "<<int(b)<<endl;//Same as above line
+return 0;
+}
