91a72f7098c292bd9aea6123633f66b4b576e32b
Ankit Patel <Itsakppatel@gmail.com>
2022-12-17 18:57:59 +0530

    Add files via upload

---
+++ Day7/dowhileExample.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i =1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=40);
+    int j =1;
+    do{
+        cout<<j<<endl;
+
+    }while(false); //Even though the condition this loop will run once
+return 0;
+}
