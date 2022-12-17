c0c4c418e8bb8f22f713597dd3b65048d35a6156
Ankit Patel <Itsakppatel@gmail.com>
2022-12-17 18:47:01 +0530

    Add files via upload

--- Day5/ReferenceVariableDemo.cpp
+++ Day5/ReferenceVariableDemo.cpp
@@ -4,9 +4,13 @@ using namespace std;
 int main(){
 
     int a =45;
-    int &b = a; 
+    int &b = a;
 
-    cout<<a<<endl;
-    cout<<b<<endl;
-    return 0;
+    cout<<"Output of a : "<<a<<endl;
+    cout<<"Output of b : "<<b<<endl;
+    b = 50;
+    cout<<"Output of a : "<<a<<endl;
+   
+
+    return 0; 
 }
