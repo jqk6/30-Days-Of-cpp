2fe3889c4e71a2fc917bfa420e3b6d406b040c98
Ankit Patel <Itsakppatel@gmail.com>
2022-12-17 22:36:44 +0530

    Add files via upload

--- Day8/pointer.cpp
+++ Day8/pointer.cpp
@@ -9,5 +9,12 @@ int main(){
 
     cout<<"The value of a at address of b is : "<<*b<<endl;
 
+    // Pointer to Pointer
+    int** c =&b;
+    cout<<"The address of b is : "<<c<<endl;
+    cout<<"The address of b is : "<<&b<<endl;
+    cout<<"The value at address c is : "<<*c<<endl;
+    cout<<"The value of address value_at(value_at(c)) is : "<<**c<<endl;
+ 
 return 0;
 }
