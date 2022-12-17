4ad51f903b88ed452dff67d2428e62953a0c9cf8
Ankit Patel <Itsakppatel@gmail.com>
2022-12-17 19:03:49 +0530

    Add files via upload

---
+++ Day7/Challenege1.cpp
@@ -0,0 +1,16 @@
+//Write a multiplication table of 6 using do-while loop
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int i = 1;
+do
+{
+    cout<<i<<" x 6 = "<<(i*6)<<endl;
+    i++;
+} while (i<=10);
+
+
+return 0;
+}
