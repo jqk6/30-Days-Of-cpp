66dd76a49be8beeb496f27904e9ef3e756a4d558
Ankit Patel <Itsakppatel@gmail.com>
2022-12-17 18:31:52 +0530

    Add files via upload

--- Day7/whileLoopExample.cpp
+++ Day7/whileLoopExample.cpp
@@ -8,5 +8,16 @@ int main(){
         i++;
     }
 
+//Infinte loop examples
+    //Example 1
+    while(true){
+        cout<<i<<endl;
+        i++;
+    }
+    //Example 2
+    while(i<=40){
+        cout<<i<<endl;
+    } 
+
 return 0;
 }
