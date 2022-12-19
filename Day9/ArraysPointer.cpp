54c1a0a468e0f98aea85615e477c227b5f92354a
Ankit Patel <Itsakppatel@gmail.com>
2022-12-19 18:34:13 +0530

    Add files via upload

--- Day9/ArraysPointer.cpp
+++ Day9/ArraysPointer.cpp
@@ -6,10 +6,17 @@ int main(){
     int* b = marks;
     cout<<"The address inside b pointer is : "<<b<<endl;
     cout<<"The address of array is : "<<marks<<endl;
-    cout<<"Value at mark[0] : "<<*b<<endl;
-    cout<<"Value at mark[1] : "<<*(b+1)<<endl;
-    cout<<"Value at mark[2] : "<<*(b+2)<<endl;
-    cout<<"Value at mark[3] : "<<*(b+3)<<endl;
+    cout<<"Value at marks[0] : "<<*b<<endl;
+    cout<<"Value at marks[1] : "<<*(b+1)<<endl;
+    cout<<"Value at marks[2] : "<<*(b+2)<<endl;
+    cout<<"Value at marks[3] : "<<*(b+3)<<endl;
+
+//Same result can be achieved with the help of loop
+    for (int i = 0; i < 4; i++)
+    {
+    cout<<"Value at marks["<<i<<"] : "<<*(b+i)<<endl;
+        
+    }
      
 return 0;
 }
