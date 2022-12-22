b851b0b7ee9a78b4a77f6782e8c483e08e94fc56
Ankit Patel <Itsakppatel@gmail.com>
2022-12-22 20:29:57 +0530

    Add files via upload

--- Day 12/Example3.cpp
+++ Day 12/Example3.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 // In this example we will see swaping values with the help reference variables
-void swapPointer(int &, int &); // function prototype
+void swapReference(int &, int &); // function prototype
 
 int main()
 {
     int a = 4, b = 5;
     cout << "The value of a is " << a << " and the value of b is " << b << endl;
-    swapPointer(a, b);
+    swapReference(a, b);
     cout << "The value of a is " << a << " and the value of b is " << b << endl;
 
     return 0;
 }
 
-void swapPointer(int &num1, int &num2)
+void swapReference(int &num1, int &num2)
 {                    // temp   num1    num2
     int temp = num1; // 4      4        5
     num1 = num2;     // 4      5        5
