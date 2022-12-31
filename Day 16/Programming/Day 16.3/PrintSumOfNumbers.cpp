67621584d6cd8db04c920ea294c37f0e6fa37485
Ankit Patel <Itsakppatel@gmail.com>
2022-12-31 22:19:45 +0530

    Add files via upload

--- Day 16/Programming/Day 16.3/PrintSumOfNumbers.cpp
+++ Day 16/Programming/Day 16.3/PrintSumOfNumbers.cpp
@@ -15,12 +15,22 @@ int main(){
 
     
     //Same program in for loop
-    
+
     int sum2=0;
     for(int i =1; i <= n; i++){
         sum2+= i;
     }
     cout<<"The sum of value from 1 to "<<n<<" is "<<sum2<<endl;
+
+    //You can verify your result with the help of formula
+    /* 
+        n(n+1)/2
+        = 5(5+1)/2
+        = (25+5)/2
+        = (30)/2
+        = 15
+
+    */
 
 return 0;
 }
---
+++ Day 16/Programming/Day 16.3/printSumOfEvenNumbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a =2;
+    int sum =0;
+    int n;
+    cout<<"Enter the value "<<endl;
+    cin>>n;
+
+    while(a<=n){
+        sum+=a;
+        a+=2;
+    }
+    cout<<"The output is "<<sum<<endl;
+
+return 0;
+}
