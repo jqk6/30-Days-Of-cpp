9ee389422804adda935a69980bb7843405f3e206
Ankit Patel <Itsakppatel@gmail.com>
2023-01-02 11:35:37 +0530

    Create pattern13.cpp

---
+++ Day 16/Programming/Day 16.4/PatternQuest/pattern13.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i =1;
+    char c ='A';
+    while(i<=4){
+        
+        int j =1;
+        while(j<=4){
+            cout<<c<<" ";
+            j++;
+        }
+        c++;
+        i++;
+        cout<<endl;
+    }
+
+return 0;
+}   
