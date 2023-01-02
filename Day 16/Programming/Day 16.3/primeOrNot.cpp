9d10659bb21133a70a1eeb4d37d0e3880ea4ae2e
Ankit Patel <Itsakppatel@gmail.com>
2023-01-02 10:20:33 +0530

    Add files via upload

--- Day 16/Programming/Day 16.3/primeOrNot.cpp
+++ Day 16/Programming/Day 16.3/primeOrNot.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a ;
-    cout<<"Entet the value "<<endl;
-    cin>>a;
-    int i =2;
-    while(i<a){
-        if(a%i == 0){
-            cout<<"The "<<a<<" number is not prime"<<endl;
+int main()
+{
+    int n;
+    cout << "Entet the value " << endl;
+    cin >> n;
+    int i = 2;
+    while (i < n)
+    {
+        if (n % i == 0)
+        {
+            cout << "The " << n << " number is not prime" << endl;
             break;
         }
         i++;
     }
-    if(i==a){
-        cout<<"The "<<a<<" number is prime"<<endl;
+    if (i == n)
+    {
+        cout << "The " << n << " number is prime" << endl;
     }
 
-return 0;
+    // Another approach to solve this problem
+    // You can take a bool and set it by default true
+    bool flag = true;
+
+    int j = 2;
+    while (j < n)
+    {
+        if (n % j == 0)
+        {
+            flag = false;
+            break;
+        }
+        j++;
+    }
+    if (flag == true)
+    {
+        cout << "The " << n << " number is prime" << endl;
+    }
+    else
+    {
+        cout << "The " << n << " number is not prime" << endl;
+    }
+
+    return 0;
 }
