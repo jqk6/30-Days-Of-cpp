ee3851d3acfaabeae95206fb81aecf1e2674874f
Ankit Patel <Itsakppatel@gmail.com>
2023-01-07 14:37:44 +0530

    Create withoutBreakInSwitch.cpp

---
+++ Day 16/Programming/Day 16.8/withoutBreakInSwitch.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num =1;
+    switch (num)
+    {
+    case 1: cout<<"Hi"<<endl;
+    case 2 : cout<<"Hello ?"<<endl;
+    case 3 : cout<<"Please reply"<<endl;
+    }
+
+return 0;
+}
