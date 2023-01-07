ef2aca182e9f56b29960bc6b4e5c6d22bd227e2d
Ankit Patel <Itsakppatel@gmail.com>
2023-01-07 20:32:28 +0530

    Create switchExpression.cpp

---
+++ Day 16/Programming/Day 16.8/switchExpression.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 2;
+    switch (4 * num)
+    {
+    case 8:
+        cout << "Inside case 1" << endl;
+        break;
+    }
+
+    return 0;
+}
